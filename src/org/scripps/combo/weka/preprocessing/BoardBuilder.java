b4eed410ae8979e79010a2ef9196cd1718d2f78a
bgood <none@none>
2012-08-30 18:56:57 -0700

    Rewrote the way boards a re managed.  Now generated en masse up front and stored in a database.  Boardroom and game operating successfully with new data.This dataset so far seems to be pretty hard..

--- src/org/scripps/combo/weka/preprocessing/BoardBuilder.java
+++ src/org/scripps/combo/weka/preprocessing/BoardBuilder.java
@@ -1,16 +1,23 @@
 package org.scripps.combo.weka.preprocessing;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Set;
 
+import org.scripps.combo.Board;
 import org.scripps.combo.weka.Weka;
+import org.scripps.combo.weka.Weka.card;
 
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
+import weka.core.Attribute;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
 
@@ -20,23 +27,62 @@ public class BoardBuilder {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String train_file = "/Users/bgood/workspace/combo/WebContent/WEB-INF/data/griffith/full_filtered_train.arff" ;
+		String train_file = "/Users/bgood/workspace/athecure/WebContent/WEB-INF/data/dream/Exprs_CNV_10yr.arff" ;
+		String metadatafile = "/Users/bgood/workspace/athecure/WebContent/WEB-INF/data/dream/Illumina2entrez.txt"; 
 		int n_per_board = 25;
-		String output_file = "/Users/bgood/workspace/combo/WebContent/WEB-INF/data/griffith/full_filtered_train_boards_by25.txt";
 		try {
-			createRandomBoards(train_file, n_per_board, output_file);
+			createRandomBoardsByGenes(train_file, metadatafile, n_per_board);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+/**
+ * Loads the database with pre-generated genesets to use for boards
+ * @param train_file
+ * @param metadatafile
+ * @param n_per_board
+ * @param outfile
+ * @throws FileNotFoundException
+ */
+	public static void createRandomBoardsByGenes(String train_file, String metadatafile, int n_per_board) throws FileNotFoundException{	
+		Weka weka = new Weka(train_file);
+		weka.loadMetadata(new FileInputStream(metadatafile));
+		//produce the list of unique entrez gene ids
+		//Set<String> gene_ids = weka.geneid_cards.keySet();
+		List<String> gene_ids = new ArrayList<String>(weka.geneid_cards.keySet());
+		System.out.println(gene_ids.size());
+		int board_id = 1;
+		Collections.shuffle(gene_ids);
+		Board board = new Board();
+		board.setPhenotype("dream_breast_cancer");
+		for(String gene : gene_ids){
+			board.getEntrez_ids().add(gene);
+			List<card> cards = weka.geneid_cards.get(gene);
+			String gene_symbol = "";
+			for(card c : cards){
+				gene_symbol = c.getName();
+				board.getAttribute_names().add(c.getAtt_name());
+			}
+			board.getGene_symbols().add(gene_symbol);
+			
+			if(board_id%n_per_board==0){
+				board.insert();
+				board = new Board();
+				board.setPhenotype("dream_breast_cancer");
+			}
+			board_id++;
+		}
 
+	}
+	
+	
 	/** 
 	 * For a given training dataset, divide up the features into random groups of 25.
 	 * Rate each of these 'boards' for difficulty based on a cross-validation test with all the features on the board
 	 * @throws FileNotFoundException 
 	 */
-	public static void createRandomBoards(String train_file, int n_per_board, String outfile) throws FileNotFoundException{
+	public static void createRandomBoardsByFeatures(String train_file, int n_per_board, String outfile) throws FileNotFoundException{
 	
 		System.out.println("board_id\tAttributes\tAtt_indexes\tcv_accuracy\tauc");
 		FileWriter f = null;
