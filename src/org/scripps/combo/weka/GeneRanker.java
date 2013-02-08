08f1f8f6d2df69195bdd77eeee68de02efee2db9
bgood <none@none>
2013-02-08 15:11:36 -0800

    fiddling with ranking and new data

--- src/org/scripps/combo/weka/GeneRanker.java
+++ src/org/scripps/combo/weka/GeneRanker.java
@@ -55,30 +55,35 @@ public class GeneRanker {
 		weka = new Weka();
 		weka.buildWeka(new FileInputStream(trainfile), null, dataset);
 	}
-	
-	
+
+
 	/**
 	 * @param args
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {		
-		Map<String, gene_rank> gid_ranked = null;
-		String dataset_1 = "dream_breast_cancer";
-		String dataset_2 = "dream_breast_cancer_2";
-		GeneRanker ranker = new GeneRanker();
+
+		String dataset = "dream_breast_cancer";
+		String outfile = "/Users/bgood/workspace/acure/database/ranking_merged_r1r2_only_cancer.txt";
 		boolean only_winning = false;
 		boolean only_cancer_people = true;
 		boolean only_bio_people = false;
 		boolean only_phd = false;
-		gid_ranked = ranker.getRankedGenes(dataset_1, only_winning, only_cancer_people, only_bio_people, only_phd);
-		gid_ranked = ranker.addReliefToGeneRanking(gid_ranked);
-		gid_ranked = ranker.scaleRelief(gid_ranked);
-		List<gene_rank> gr = new ArrayList<gene_rank>(gid_ranked.values());
+		GeneRanker ranker = new GeneRanker();
+		Map<String, gene_rank> gid_ranked_1 = null;
+		gid_ranked_1 = ranker.getRankedGenes(dataset, only_winning, only_cancer_people, only_bio_people, only_phd);
+		dataset = "dream_breast_cancer_2";
+		Map<String, gene_rank> gid_ranked_2 = null;
+		gid_ranked_2 = ranker.getRankedGenes(dataset, only_winning, only_cancer_people, only_bio_people, only_phd);
+		List<gene_rank> gr = ranker.mergeGeneRankings(gid_ranked_1, gid_ranked_2);
+		ranker.writeFile(outfile, gr);
+
+		//List<gene_rank> gr = ranker.exportGeneRankings(dataset, outfile, only_winning, only_cancer_people, only_bio_people, only_phd);
 		Collections.shuffle(gr);
 		//gr = ranker.sortByRelief(gr);
 		//gr = ranker.sortByReliefAndFrequency(gr);
-		String train_file = "/Users/bgood/workspace/acure/WebContent/WEB-INF/data/dream/Exprs_CNV_lts_2500genes.arff";
-		ranker.initWeka(train_file, dataset_2);
+		//String train_file = "/Users/bgood/workspace/acure/WebContent/WEB-INF/data/dream/Exprs_CNV_lts_2500genes.arff";
+		//ranker.initWeka(train_file, dataset);
 
 		int runs = 125;
 		System.out.println("N_genes\tcv_score");
@@ -101,10 +106,67 @@ public class GeneRanker {
 					test_fs.add(gr.get(i).entrez);
 				}
 			}		
-			float cv = ranker.testGeneList(test_fs, dataset_2, false, model);
+			float cv = ranker.testGeneList(test_fs, dataset, false, model);
 			System.out.println(test_fs.size()+"\t"+cv);
 		}
 
+	}
+
+	public List<gene_rank> mergeGeneRankings(Map<String, gene_rank> r1, Map<String, gene_rank> r2){
+		List<gene_rank> merged = new ArrayList<gene_rank>();
+		Set<String> genes = new HashSet<String>(r1.keySet());
+		genes.addAll(r2.keySet());
+		for(String gene : genes){
+			gene_rank rank1 = r1.get(gene);
+			gene_rank rank2 = r2.get(gene);
+			gene_rank merg = mergeRanks(rank1, rank2);
+			merged.add(merg);
+		}
+		return merged;
+	}
+
+	public gene_rank mergeRanks(gene_rank r1, gene_rank r2){
+		gene_rank merged  = new gene_rank();
+		if(r1!=null){
+			merged.entrez = r1.entrez;
+			merged.f_id = r1.f_id;
+			merged.symbol = r1.symbol;
+			merged.views = r1.views;
+			merged.votes = r1.votes;
+		}else if(r2!=null){
+			merged.entrez = r2.entrez;
+			merged.symbol = r2.symbol;
+			merged.f_id = r2.f_id;
+			merged.views = 0;
+			merged.votes = 0;
+		}else{
+			return null;
+		}
+		if(r2!=null){
+			merged.views+=r2.views;
+			merged.votes+=r2.votes;
+		}
+		return merged;
+	}
+
+	/**
+	 * Get the gene information for a given dataset
+	 * @param dataset
+	 * @param outfile
+	 * @param only_winning
+	 * @param only_cancer_people
+	 * @param only_bio_people
+	 * @param only_phd
+	 * @return
+	 */
+	public List<gene_rank> exportGeneRankings(String dataset, String outfile, boolean only_winning, boolean only_cancer_people, boolean only_bio_people, boolean only_phd){
+		Map<String, gene_rank> gid_ranked = null;
+		gid_ranked = getRankedGenes(dataset, only_winning, only_cancer_people, only_bio_people, only_phd);
+		gid_ranked = addReliefToGeneRanking(gid_ranked);
+		gid_ranked = scaleRelief(gid_ranked);
+		List<gene_rank> gr = new ArrayList<gene_rank>(gid_ranked.values());
+		writeFile(outfile, gr);
+		return gr;
 	}
 
 	/**
@@ -153,7 +215,7 @@ public class GeneRanker {
 			float cv = testGeneList(test_fs, dataset, printout, model);
 			System.out.println(run+"\t"+cv);
 		}
-		
+
 	}
 
 	public List<gene_rank> sortByRelief(List<gene_rank> ranked){
@@ -168,7 +230,7 @@ public class GeneRanker {
 		Collections.sort(ranked, MergeOrder);
 		return ranked;
 	}
-	
+
 	public List<gene_rank> sortByReliefAndFrequency(List<gene_rank> ranked){
 		//sort in descending order of the addition of relief and selection frequency
 		Comparator<gene_rank> MergeOrder =  new Comparator<gene_rank>() {
@@ -185,7 +247,7 @@ public class GeneRanker {
 		Collections.sort(ranked, MergeOrder);
 		return ranked;
 	}
-	
+
 	/**
 	 * Divide all the relief values by the max to get a -1 to 1 scale.
 	 * @param gid_ranked
@@ -221,7 +283,7 @@ public class GeneRanker {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public gene_rank makeGene_rank(){
 		gene_rank r = new gene_rank();
 		r.frequency = 0;
@@ -274,10 +336,10 @@ public class GeneRanker {
 				}
 			}
 		}
-		
+
 		return gene_ranked;
 	}
-	
+
 
 	/**
 	 * Get data for ranking genes based on selection frequency
@@ -385,13 +447,13 @@ public class GeneRanker {
 			}
 			handsall = hands;
 		}
-		
+
 		if(!only_cancer_people&&!only_bio_people&&!only_phd){
 			hands = handsall;
 		}
 		return hands;
 	}
-	
+
 	/**
 	 * Build a voting classifier, each completed hand is used to make a decision tree and is given one vote
 	 * classifications made using majority rule.  Simple version of bagging that is similar but less
@@ -428,7 +490,7 @@ public class GeneRanker {
 		cv = short_result.getAccuracy();
 		return cv;
 	}
-	
+
 	/**
 	 * run a feature id list through weka and see what comes out
 	 * these ids are local to our database
@@ -457,7 +519,7 @@ public class GeneRanker {
 		return cv;
 	}
 
-	
+
 	/**
 	 * Get an id what random sampling would look like
 	 * @param n_genes
@@ -466,38 +528,38 @@ public class GeneRanker {
 	 * @param train_file
 	 * @throws Exception
 	 */
-		public static void generateRandomBaseline(int n_genes, int n_sampled, String dataset, String train_file) throws Exception{
-			//get results vector ready
-			DescriptiveStatistics cvs = new DescriptiveStatistics();
-			Weka weka = new Weka();
-			weka.buildWeka(new FileInputStream(train_file), null, dataset);
-			List<String> all_genes = new ArrayList<String>(weka.getFeatures().keySet());
+	public static void generateRandomBaseline(int n_genes, int n_sampled, String dataset, String train_file) throws Exception{
+		//get results vector ready
+		DescriptiveStatistics cvs = new DescriptiveStatistics();
+		Weka weka = new Weka();
+		weka.buildWeka(new FileInputStream(train_file), null, dataset);
+		List<String> all_genes = new ArrayList<String>(weka.getFeatures().keySet());
 
-			//      go through n_sampled random combos
-			for(int i = 0; i< n_sampled; i++) {
-				Collections.shuffle(all_genes);
-				List<String> group = new ArrayList<String>(n_sampled);
-				int n = 0;
-				for(String g : all_genes){
-					group.add(g);
-					if(n>=n_genes){
-						break;
-					}
-					n++;
+		//      go through n_sampled random combos
+		for(int i = 0; i< n_sampled; i++) {
+			Collections.shuffle(all_genes);
+			List<String> group = new ArrayList<String>(n_sampled);
+			int n = 0;
+			for(String g : all_genes){
+				group.add(g);
+				if(n>=n_genes){
+					break;
 				}
-				List<String> geneids = new ArrayList<String>();
-				String genes = "";
-				for(String gh : group){
-					geneids.add(gh);
-					genes+=gh+",";
-				}
-				//test group
-				execution base = weka.pruneAndExecuteWithUniqueIds(geneids, null, dataset);
-				double cv = base.eval.pctCorrect();
-				cvs.addValue(cv);
-				System.out.println(i+"\t"+cv);
+				n++;
 			}
-
-			System.out.println(cvs.toString());
+			List<String> geneids = new ArrayList<String>();
+			String genes = "";
+			for(String gh : group){
+				geneids.add(gh);
+				genes+=gh+",";
+			}
+			//test group
+			execution base = weka.pruneAndExecuteWithUniqueIds(geneids, null, dataset);
+			double cv = base.eval.pctCorrect();
+			cvs.addValue(cv);
+			System.out.println(i+"\t"+cv);
 		}
+
+		System.out.println(cvs.toString());
+	}
 }
