4ca5c92a598ec93317a836726af9bd2cd505369e
gkarthik_92 <xraydiffraction@gmail.com>
2013-09-06 21:16:26 +0530

    Delete Working along with storing previous Attributes.

--- WebContent/cured3/assets/js/app.js
+++ WebContent/cured3/assets/js/app.js
@@ -288,10 +288,10 @@ NodeView = Backbone.Marionette.ItemView.extend({
 	},
 	events : {
 		'click button.addchildren' : 'addChildren',
-		'click button.delete' : 'remove',
+		'click button.delete' : 'removeChildren',
 		'dblclick .name' : 'edit',
 		'keypress .edit' : 'updateOnEnter',
-		'blur .edit' : 'close'
+		'blur .edit' : 'closeInput'
 	},
 	initialize : function() {
 		_.bindAll(this, 'remove', 'addChildren');
@@ -323,10 +323,10 @@ NodeView = Backbone.Marionette.ItemView.extend({
 	},
 	updateOnEnter : function(e) {
 		if (e.which == 13) {
-			this.close();
+			this.closeInput();
 		}
 	},
-	close : function() {
+	closeInput : function() {
 		var value = this.ui.input.val().trim();
 		if (value) {
 			this.model.set('name', value);
@@ -339,9 +339,20 @@ NodeView = Backbone.Marionette.ItemView.extend({
 	},
 	remove : function() {
 		//if (Cure.NodeCollection.length > 1) {//Have to eliminate use of Cure.NodeCollection
-			$(this.el).remove();
+		this.$el.remove();
 			Cure.delete_all_children(this.model);
 			this.model.destroy();
+		//}
+	},
+	removeChildren : function() {
+		//if (Cure.NodeCollection.length > 1) {//Have to eliminate use of Cure.NodeCollection
+			Cure.delete_all_children(this.model);
+			var prevAttr = this.model.get("previousAttributes");
+			console.log(prevAttr)
+				for ( var key in prevAttr) {
+						this.model.set(key, prevAttr[key]);
+				}
+				this.model.set("previousAtributes", []);
 		//}
 	},
 	addChildren : function() {
@@ -376,6 +387,7 @@ AddRootNodeView = Backbone.Marionette.ItemView.extend({
 	    }catch (exception) {}
 	    if(kind_value=="leaf_node")
 	    {
+	    	model.set("previousAttributes",model.toJSON());
 	    	model.set("name",ui.item.name);
 	    	model.set("options",{id: ui.item.id,"kind": "split_node"});
 	    }
