92be153c0f4dd4e0575dad06024c7ee3a7f3b1fa
Karthik <xraydiffraction@gmail.com>
2014-03-17 18:18:42 -0700

    Put collaborator check while saving. No saving without adding nodes.

--- WebContent/cure2.0/app/views/CommentView.js
+++ WebContent/cure2.0/app/views/CommentView.js
@@ -25,12 +25,15 @@ CommentView = Backbone.Marionette.ItemView.extend({
 		this.model.bind('change', this.render);
 	},
 	initSave: function(){
-		if(Cure.PlayerNodeCollection.length>0){
+		if(Cure.PlayerNodeCollection.length>0 && Cure.CollaboratorCollection.pluck("id").indexOf(cure_user_id)!=-1){
 			this.model.set("editView",1);
-		} else {
+		} else if(Cure.PlayerNodeCollection.length==0) {
 			Cure.utils
 	    .showAlert("<strong>Empty Tree!</strong><br>Use the autocomplete box to build a tree.", 0);
-		}
+		} else if(Cure.CollaboratorCollection.pluck("id").indexOf(cure_user_id)==-1) {
+      Cure.utils
+      .showAlert("<strong>You haven't added anything!</strong><br>Please do not save trees built by other users directly.", 0);
+		} 
 	},
 	saveTree: function(){
 		this.model.set("content",$(this.ui.commentContent).val());
