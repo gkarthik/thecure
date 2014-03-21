6cc3b900db72a513146cd79a19e4cca36fb774c2
Karthik <xraydiffraction@gmail.com>
2014-03-21 13:26:57 -0700

    Score bar chart working for enter() and exit(). Must add scroll

--- WebContent/cure2.0/app/views/ScoreView.js
+++ WebContent/cure2.0/app/views/ScoreView.js
@@ -9,8 +9,8 @@ define([
     ], function($, Marionette, d3, scoreTemplate, scoreChangeTemplate) {
 ScoreView = Backbone.Marionette.ItemView.extend({
 	initialize : function() {
-		_.bindAll(this, 'updateScore');
-		this.model.bind("change", this.updateScore);
+		_.bindAll(this, 'updateScore', 'updateAxis');
+		this.model.bind("change:size", this.updateScore);
 	},
 	ui : {
 		'svg' : "#ScoreSVG",
@@ -59,27 +59,51 @@ ScoreView = Backbone.Marionette.ItemView.extend({
 	splitNodeArray: [],
 	updateAxis: function(){
 		var thisModel = this.model;
-		var xLimit = 10;
 		var yLimit = 80000;
 		var xLength = Cure.Scorewidth-50;
 		var tempSplitNodeArray = Cure.PlayerNodeCollection.getSplitNodeArray();
 		var splitNodeArray = this.splitNodeArray;
 		var pushCount = 0;
+		for(var temp in splitNodeArray){
+			var i = tempSplitNodeArray.length;
+			while( i-- ) {
+			    if( tempSplitNodeArray[i].cid ==  splitNodeArray[temp].cid ) break;
+			}
+			if(i==-1){
+				splitNodeArray.splice(temp,1);
+			}
+		}
 		for(var temp in tempSplitNodeArray){
-			if(this.splitNodeArray.indexOf(tempSplitNodeArray[temp].name)==-1){
-				this.splitNodeArray.push(tempSplitNodeArray[temp].name);
+			var i = splitNodeArray.length;
+			while( i-- ) {
+			    if( splitNodeArray[i].cid ==  tempSplitNodeArray[temp].cid ) break;
+			}
+			if(i==-1){
+				splitNodeArray.push(tempSplitNodeArray[temp]);
 				pushCount++;
 			}
 		}
-		if(thisModel.get('size')>0){
-			xLimit = splitNodeArray.length+1;
-			yLimit = thisModel.get('score')+10000;
-			xLength = splitNodeArray.length * 50;
+		if(pushCount > 1) {
+			splitNodeArray.splice(0,splitNodeArray.length-1);
 		}
-		var	xAxisScale = d3.scale.ordinal().domain(splitNodeArray.map(function(d){ return d;})).rangeRoundBands([0, xLength], 1);
-		var yAxisScale = d3.scale.linear().domain([0, yLimit]).range([Cure.Scorewidth-30, 0]);
+		if(splitNodeArray.length>0){
+			splitNodeArray[splitNodeArray.length-1].score = [parseFloat(1000 * this.model.get('pct_correct')), parseFloat(750 * (1/this.model.get('size'))), parseFloat(500 * this.model.get('novelty'))];
+		}
+		
+		if(thisModel.get('size')>0){
+			yLimit = this.model.get('score')+10000;
+			xLength = (splitNodeArray.length+1) * 50;
+		}
+		var	xAxisScale = d3.scale.ordinal().domain(splitNodeArray.map(function(d){ return d.cid;})).rangeRoundBands([0, xLength], 1);
+		var yAxisScale = d3.scale.linear().domain([0, yLimit]).range([Cure.Scoreheight-30, 0 ]);
 		//Update axis
-		var xAxis = d3.svg.axis().scale(xAxisScale).orient("bottom");
+		var xAxis = d3.svg.axis().scale(xAxisScale).orient("bottom").tickFormat(function(d){
+			var i = splitNodeArray.length;
+			while( i-- ) {
+		    if( splitNodeArray[i].cid ==  d ) break;
+			}
+			return splitNodeArray[i].name; 
+		});
 		var yAxis = d3.svg.axis().scale(yAxisScale).orient("left").tickFormat(function (d) {
     	var prefix = d3.formatPrefix(d);
     	return prefix.scale(d) + "k";
@@ -87,23 +111,25 @@ ScoreView = Backbone.Marionette.ItemView.extend({
 		this.xaxis.transition().duration(Cure.duration).call(xAxis);
 		this.yaxis.transition().duration(Cure.duration).call(yAxis);
 		
-		this.SVG.append("svg:g").append("rect")
-		.attr("width",50)
-		.attr("x",function(d){
-			return xAxisScale(splitNodeArray[splitNodeArray.length-1]);//Same Name Might cause problem!
-		})
-		.attr("y",function(){
-			return Cure.Scoreheight-20;
-		})
-		.attr("height",function(){
-			return 750 * (1 / thisModel.get("size"));
-		}).style("fill","red");
-		//750 * (1 / this.get("size")) + 500* this.get("novelty") + 1000 * this.get("pct_correct");
+		var layer = this.SVG.selectAll(".layer").data(splitNodeArray).enter().append("g").attr("class","layer")
+		.attr("transform",function(d){
+			return "translate("+parseFloat(xAxisScale(d.cid)+15)+",0)";
+		});
 		
+		var y=Cure.Scoreheight-20;
+		layer.selectAll("scoreRect").data(function(d){return d.score; }).enter().append("rect").attr("class","scoreRect")
+		.attr("width",30)
+    .style("fill", function(d, i) { return Cure.colorScale(i); })
+    .attr("y", function(d) { return y; })
+    .attr("height", 0)
+		.transition().duration(Cure.duration)
+		.attr("y", function(d) { y-=yAxisScale(yLimit-d);return y; })
+    .attr("height", function(d) { return yAxisScale(yLimit-d); });
+		
+		this.SVG.selectAll(".layer").data(splitNodeArray).exit().transition().duration(Cure.duration).attr("height",0).remove();
 	},
 	updateScore : function() {
 		this.updateAxis();
-		//Draw Rect
 		$(this.ui.scoreEL).html(this.model.get("score"));
 	},
 	onShow: function(){
