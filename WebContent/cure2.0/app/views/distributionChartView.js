3e250a493d39aed96571d922f0b35932ed913750
karthik <none@none>
2014-05-15 15:10:14 -0700

    Drag limits on .splitPointDrag

--- WebContent/cure2.0/app/views/distributionChartView.js
+++ WebContent/cure2.0/app/views/distributionChartView.js
@@ -148,6 +148,17 @@ DistChartView = Marionette.ItemView.extend({
 				xLength = xLength - ((globalWidth-40)/(plotValues.length));
 				var splitScale = d3.scale.linear().domain([plotValues[0].value, plotValues[plotValues.length-1].value]).range([0,xLength]);
 				var reverseSplitScale = d3.scale.linear().domain([0,xLength]).range([plotValues[0].value, plotValues[plotValues.length-1].value]);
+				var splitPointIndicator = SVG.append("g").attr("class","split_point_indicator")
+				.attr("transform",function(){
+					var translateX = 30 + ((globalWidth-40)/(plotValues.length*2)) + parseFloat(splitScale(splitPoint));
+					var total = 0;
+					for(var i in plotValues[temp].frequency){
+						total += plotValues[temp].frequency[i];
+					}
+					return "translate("+translateX+",10)";
+				});
+				splitPointIndicator.append("svg:rect").attr("height",globalHeight-40).attr("width",2).attr("fill","green");
+				splitPointIndicator.append("svg:text").attr("fill","green").text(Math.round(splitPoint*100)/100).attr("text-anchor","middle").style("font-size","10px");
 				var splitPointGroup = SVG.append("g").attr("class","split_point")
 				.attr("transform",function(){
 					var translateX = 30 + ((globalWidth-40)/(plotValues.length*2)) + parseFloat(splitScale(splitPoint));
@@ -157,7 +168,6 @@ DistChartView = Marionette.ItemView.extend({
 					}
 					return "translate("+translateX+",10)";
 				});
-				
 				var origin = [0,0];
 				var drag = d3.behavior.drag().origin(function() { 
 					var t = d3.select(this).attr("transform");
@@ -169,21 +179,20 @@ DistChartView = Marionette.ItemView.extend({
 	        this.__customorigin__ = { x: parseFloat(origin[0]), y: parseFloat(origin[1]) };
 	      }).on("drag", function(){
 	      	var o = this.__customorigin__;
-	        var dist = parseFloat(d3.event.x);
-	        splitPoint = reverseSplitScale(dist-30-((globalWidth-40)/(plotValues.length*2)));
+	        var min = 30+((globalWidth-40)/(plotValues.length*2));
+	        splitPoint = reverseSplitScale(Math.min(Math.max(d3.event.x-min, 0), xLength));
 	        SVG.selectAll(".splitValueLabel").text(Math.round(splitPoint*100)/100);
-	        d3.select(this).attr("transform","translate("+d3.event.x+","+o.y+")");
+	        var translateX =  Math.min(min+xLength,  Math.max(min,  d3.event.x));
+	        d3.select(this).attr("transform","translate("+translateX+","+o.y+")");
 		    }).on("dragend",function(){
 		    	var options = thisModel.get('splitNode').get('options');
 		    	options.set('split_point', splitPoint);
 		    	Cure.PlayerNodeCollection.sync();
 		    	delete this.__customorigin__;
 		    });
-		    
 				SVG.select(".split_point").call(drag);
-				
 				splitPointGroup.append("svg:rect").attr("height",globalHeight-40).attr("width",2).attr("fill","steelblue");
-				splitPointGroup.append("svg:text").attr("class","splitValueLabel").attr("fill","steelblue").text(Math.round(splitPoint*100)/100).attr("text-anchor","middle").style("font-size","10px");
+				splitPointGroup.append("svg:text").attr("class","splitValueLabel").attr("fill","steelblue").text(Math.round(splitPoint*100)/100).attr("text-anchor","middle").style("font-size","10px");				
 				var dragHolder = splitPointGroup.append("svg:g").attr("transform","translate(-18,"+((globalHeight-40)/2)+")").attr("class","dragSplitPoint");
 				dragHolder.append("svg:rect").attr("height",15).attr("width",40).attr("fill","steelblue").attr("transform","translate(0,-10)");
 				dragHolder.append("svg:text").attr("fill","white").text("DRAG").style("font-size","10px").attr("transform","translate(2,2)");
