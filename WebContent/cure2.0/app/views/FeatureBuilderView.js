8b8cf4706744ae9297b6529ef23c8951b6c598a2
karthik <none@none>
2014-05-31 00:32:17 -0700

    Feature Builder textbox gene tagging primary draft complete.
    --HG--
    branch : featurebuilder

--- WebContent/cure2.0/app/views/FeatureBuilderView.js
+++ WebContent/cure2.0/app/views/FeatureBuilderView.js
@@ -10,14 +10,45 @@ define([
     ], function($, Marionette, FeatureBuilderTemplate) {
 FeatureBuilderView = Marionette.ItemView.extend({
 	initialize : function() {
-		
+		$.valHooks.textarea = {
+			get: function( elem ) {
+				return elem.value;
+			}
+		};
 	},
 	template: FeatureBuilderTemplate,
 	ui : {
-		'featureExpression': '#feature-expression'
+		'featureExpression': '#feature-expression',
+		'hiddenSpan': '#hidden-span'
 	},
 	events:{
-		'click #build-feature': 'buildFeature'
+		'click #build-feature': 'buildFeature',
+		'keyup #feature-expression': 'checkAndTagText'
+	},
+	checkAndTagText: function(e){
+		if($(this.ui.featureExpression).get(0).scrollHeight-11 > $(this.ui.featureExpression).height()){
+			$(this.ui.featureExpression).css({'height': $(this.ui.featureExpression).get(0).scrollHeight+5});
+		}
+		var value = $(this.ui.featureExpression).val();
+		var keyword = "EGR3";
+		var index = value.toUpperCase().indexOf(keyword);
+		if(index!=-1){
+			this.addTag(value,index,keyword);
+		}
+	},
+	addTag: function(value,index,keyword){
+		var textBeforeKey = value.substring(0,index);
+		$(this.ui.hiddenSpan).html(textBeforeKey+keyword);
+		var pos = [];
+		pos[1] = $(this.ui.hiddenSpan).innerHeight();
+		var lines = textBeforeKey.split("\n");
+		$(this.ui.hiddenSpan).html(lines[lines.length-1]);
+		pos[0] = $(this.ui.hiddenSpan).width()+1;
+		console.log(pos);
+		//var pos = [$(this.ui.hiddenSpan).width(),$(this.ui.hiddenSpan).height()]
+		this.$el.append("<span id='"+keyword+"-tag' class='highlight-tag'>"+value.substring(index,index+keyword.length)+"</span>");
+		var tag = $("#"+keyword+"-tag");
+		tag.css({'left':pos[0],'top':pos[1]-tag.height()});
 	},
 	buildFeature: function(){
 		var feature_exp = $(this.ui.featureExpression).val();
